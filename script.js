5749f4bf93e841c1c541a79db6228133518d28f9
julliacarvalhooliveira-alt <jullia.carvalho.oliveira@escola.pr.gov.br>
2026-09-23 14:28:04 -0300

    atualizei o meu codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = documento.querySelectorAll(".curtir")
+botoesCurtir.forEach(function(botaoCurtir) {
+    let curtiu = false;
+    botaoCurtir.addEventlistener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;
+        } else{
+            contador.textContent--;
+            curtiu = false;
+        }
+    }
+}
